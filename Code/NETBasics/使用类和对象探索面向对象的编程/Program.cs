1d94a5f75ab8f5e5ed72bfdeb1f68489a16b629b
杜文龙 <858594355@qq.com>
2019-06-20 21:58:34 +0800

    增加教程地址。

--- Code/NETBasics/使用类和对象探索面向对象的编程/Program.cs
+++ Code/NETBasics/使用类和对象探索面向对象的编程/Program.cs
@@ -10,6 +10,9 @@ namespace 使用类和对象探索面向对象的编程
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------教程地址: ------------------------"); 
+            Console.WriteLine("https://docs.microsoft.com/zh-cn/dotnet/csharp/tutorials/intro-to-csharp/introduction-to-classes");
+            Console.WriteLine("--------------------------------------------------------------");
             var account = new BankAccount("杜文龙", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} $ initial balance.");
             account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
