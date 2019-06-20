c1a35a7464ebe467ede7a413a14a9297850319c6
858594355 <858594355@qq.com>
2019-06-20 20:39:53 +0800

    学习NET C# 基础。OOP。WCF

---
+++ Code/NETBasics/使用类和对象探索面向对象的编程/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 使用类和对象探索面向对象的编程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
