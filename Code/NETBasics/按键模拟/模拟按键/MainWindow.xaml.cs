52537e37dd0ac2986444c66897ab6be77f6e8d35
杜文龙 <858594355@qq.com>
2019-07-04 00:42:33 +0800

    封装大漠插件 相关代码。

---
+++ Code/NETBasics/按键模拟/模拟按键/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 模拟按键
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dm.dmsoft dm = new Dm.dmsoft();
+      
+        public MainWindow()
+        {
+            InitializeComponent();
+            AutoRegCom("regsvr32 -s dm.dll");
+           
+        }
+        static string AutoRegCom(string strCmd)
+        {
+            string rInfo;
+             
+            try
+            {
+                Process myProcess = new Process();
+                ProcessStartInfo myProcessStartInfo = new ProcessStartInfo("cmd.exe");
+                myProcessStartInfo.UseShellExecute = false;
+                myProcessStartInfo.CreateNoWindow = true;
+                myProcessStartInfo.RedirectStandardOutput = true;
+                myProcess.StartInfo = myProcessStartInfo;
+                myProcessStartInfo.Arguments = "/c " + strCmd;
+                myProcess.Start();
+                StreamReader myStreamReader = myProcess.StandardOutput;
+                rInfo = myStreamReader.ReadToEnd();
+                myProcess.Close();
+                rInfo = strCmd + "\r\n" + rInfo;
+                return rInfo;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dm.MoveTo(30, 30);
+        }
+    }
+
+}
