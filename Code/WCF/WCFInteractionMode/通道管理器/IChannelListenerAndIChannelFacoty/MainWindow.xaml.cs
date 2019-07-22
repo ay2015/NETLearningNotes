c1657ff25528872533eab2e04b701108ec852172
858594355 <858594355@qq.com>
2019-07-22 17:58:22 +0800

    通道管理器

---
+++ Code/WCF/WCFInteractionMode/通道管理器/IChannelListenerAndIChannelFacoty/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IChannelListenerAndIChannelFacoty
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NetTcpBinding binding;
+        public MainWindow()
+        {
+            InitializeComponent();
+            binding = new NetTcpBinding();//创建绑定
+            IChannelFactory<IRequestChannel> factory = binding.BuildChannelFactory<IRequestChannel>(new BindingParameterCollection());
+            factory.Open();//打开ChannelFactory
+            //这里创建IRequestChannel
+            IRequestChannel requestChannel = factory.CreateChannel(new EndpointAddress("http://localhost:9090/RequestReplyService"));
+
+        }
+    }
+}
