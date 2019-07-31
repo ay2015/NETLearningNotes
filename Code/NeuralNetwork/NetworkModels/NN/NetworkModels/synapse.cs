879005e3d36614d1ac71f35a2137a3347e0da97c
858594355 <858594355@qq.com>
2019-07-31 16:28:06 +0800

    增加NN临近模型算法

---
+++ Code/NeuralNetwork/NetworkModels/NN/NetworkModels/synapse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN.NetworkModels
+{
+    /// <summary>
+    /// 突触,它是一种将一个神经元连接到另外一个神经元，已经容纳权重和权重增量的容器
+    /// </summary>
+    public class synapse
+    {
+        #region  -- Properties --
+        /// <summary>
+        ///   Gets or sets the identifier.
+        ///   读取或设置这个标识。
+        /// </summary>
+        public Guid Id { get; set; }
+
+       
+        #endregion
+    }
+}
